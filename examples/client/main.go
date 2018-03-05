d833a9742909390e069200c33ba9effb70613d48
Florent Messa <florent.messa@gmail.com>
2018-03-05 22:47:18 +0100

    feat: add retry middleware

--- examples/client/main.go
+++ examples/client/main.go
@@ -6,19 +6,30 @@ import (
 	"log"
 	"time"
 
+	grpc_retry "github.com/grpc-ecosystem/go-grpc-middleware/retry"
 	"github.com/ulule/ipfix/proto"
+	"golang.org/x/net/trace"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 )
 
 func main() {
 	var serverAddr string
 	var ipAddress string
+	var maxRetry uint
 
 	flag.StringVar(&serverAddr, "server.addr", "127.0.0.1:33001", "rpc server addr")
 	flag.StringVar(&ipAddress, "ip", "127.0.0.1", "ip address")
+	flag.UintVar(&maxRetry, "retries", 3, "max retries")
 	flag.Parse()
 
-	conn, err := grpc.Dial(serverAddr, grpc.WithInsecure())
+	tr := trace.New("ipfix.Client", "GetLocation")
+	defer tr.Finish()
+
+	conn, err := grpc.Dial(serverAddr,
+		grpc.WithInsecure(),
+		grpc.WithUnaryInterceptor(grpc_retry.UnaryClientInterceptor()),
+	)
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
@@ -27,7 +38,12 @@ func main() {
 
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
-	loc, err := c.GetLocation(ctx, &proto.GetLocationRequest{IpAddress: ipAddress})
+	ctx = trace.NewContext(ctx, tr)
+	loc, err := c.GetLocation(ctx, &proto.GetLocationRequest{IpAddress: ipAddress},
+		grpc_retry.WithMax(maxRetry),
+		grpc_retry.WithPerRetryTimeout(1*time.Second),
+		grpc_retry.WithCodes(codes.Unavailable))
+
 	if err != nil {
 		log.Fatalf("could not retrieve result: %v", err)
 	}
